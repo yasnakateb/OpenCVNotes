3e3e4652603afbb0363660f68ba5feaf545c75f1
yasnakateb <yasnakatebzadeh@gmail.com>
2022-05-03 17:01:39 +0430

    Add shapes

--- src/test.cpp
+++ src/test.cpp
@@ -11,8 +11,8 @@ void loadImage(std::string path)
     cv::waitKey(0); 
 }
 
-void loadVideo(std::string path){
-
+void loadVideo(std::string path)
+{
     cv::VideoCapture cap(path);
     cv::Mat img;
 
@@ -86,6 +86,54 @@ void cannyEdgeDetector(std::string path)
     cv::waitKey(0); 
 }
 
+void dilateOrErodeImage(std:: string path, bool dilateImg)
+{
+    cv::Mat imgBlur, imgCanny, imgOutput;
+
+    cv::Mat img = cv::imread(path);
+
+    cv::Mat kernel = getStructuringElement(cv::MORPH_RECT, cv::Size(1,1));
+
+    cv::GaussianBlur(img, imgBlur, cv::Size(3,3), 5, 0);
+    Canny(imgBlur, imgCanny, 25, 75);
+    if (dilateImg)
+    {
+        dilate(imgCanny, imgOutput, kernel);
+    }
+    else
+    {
+        erode(imgCanny, imgOutput, kernel);
+    }
+    
+    imshow("Output Image", imgOutput);
+    cv::waitKey(0); 
+}
+
+void reSizeImage(std::string path, float scaleX, float scaleY) 
+{
+    cv::Mat img = cv::imread(path);
+    cv::Mat imgResize;
+    
+    resize(img, imgResize, cv::Size(), scaleX, scaleY);
+
+    imshow("Original Image", img);
+    imshow("Resize Image", imgResize);
+    cv::waitKey(0); 
+}
+
+void cropImage(std::string path, int _x, int _y, int _width, int _height) 
+{
+    cv::Mat img = cv::imread(path);
+    cv::Mat imgCrop;
+    
+    cv::Rect roi(_x, _y, _width, _height);
+    imgCrop = img(roi);
+
+    imshow("Original Image", img);
+    imshow("Crop Image", imgCrop);
+    cv::waitKey(0); 
+}
+
 int main(int, char**) 
 {
    // loadImage("assets/goat.jpg");
@@ -93,5 +141,18 @@ int main(int, char**)
    // convertToGray("assets/goat.jpg");
    // blurImage("assets/goat.jpg");
    // cannyEdgeDetector("assets/goat.jpg");
+   // dilateOrErodeImage("assets/goat.jpg", false);
+   // reSizeImage("assets/goat.jpg", 0.5, 0.7);
+   // cropImage("assets/goat.jpg", 1, 1, 50, 50); 
+
+   // Shape
+   cv::Mat img(512, 512, CV_8UC3, cv::Scalar(250, 100, 250));
+   circle (img, cv::Point (256, 256), 155, cv::Scalar(0, 69, 255), cv::FILLED);
+   rectangle(img, cv::Point(130, 226), cv::Point(382, 286), cv::Scalar(255, 255, 255), cv::FILLED);
+   line(img, cv::Point(130, 296), cv::Point(382, 296), cv::Scalar(255, 255, 255), 2);
+   putText(img, "Hi Yasna!", cv::Point(207, 262), cv::FONT_HERSHEY_DUPLEX, 0.75, cv::Scalar(0, 69, 255), 2);
+
+   imshow("Original Image", img);
+   cv::waitKey(0); 
    return 0;
 }
